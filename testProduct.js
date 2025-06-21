cacbc3775259fde7062fdf4553da4aa57786a4e0
YKLee98 <math.makes.you.free@gmail.com>
2025-06-21 18:27:24 +0900

    Update testProduct.js

--- testProduct.js
+++ testProduct.js
@@ -2,7 +2,7 @@
 const orderService = require('./src/services/orderService');
 
 async function test() {
-  const result = await orderService.testBunjangProductOrder('332047857');
+  const result = await orderService.testBunjangProductOrder('');
   console.log(JSON.stringify(result, null, 2));
   process.exit(0);
 }
